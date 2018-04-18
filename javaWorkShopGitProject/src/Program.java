dc705fdb82e60a75be407603c303acd06c5c2f22
DavidPrades <davidprades8@gmail.com>
2018-04-18 19:36:46 +0200

    Create classes of our project

---
+++ javaWorkShopGitProject/src/Program.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * 2018, All rights reserved.
+ *******************************************************************************/
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of Program.
+ * 
+ * @author alu20908719v
+ */
+public class Program {
+	// Start of user code (user defined attributes for Program)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public Program() {
+		// Start of user code constructor for Program)
+		super();
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for Program)
+
+	// End of user code
+
+}
